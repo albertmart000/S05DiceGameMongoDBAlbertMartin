ee209e9b5c8f50867ad0bf32058344e9491318d5
albertmart000 <albertmart000@yahoo.es>
2023-01-30 09:48:14 +0100

    Code Ok

--- src/main/java/cat/itacademy/barcelonactiva/S05DiceGameMongoDBAlbertMartin/model/domain/Dice.java
+++ src/main/java/cat/itacademy/barcelonactiva/S05DiceGameMongoDBAlbertMartin/model/domain/Dice.java
@@ -1,5 +1,6 @@
 package cat.itacademy.barcelonactiva.S05DiceGameMongoDBAlbertMartin.model.domain;
 
+
 public class Dice {
 
     public static Integer throwDice() {
--- src/main/java/cat/itacademy/barcelonactiva/S05DiceGameMongoDBAlbertMartin/model/repository/IPlayerRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/S05DiceGameMongoDBAlbertMartin/model/repository/IPlayerRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 @Component
 public interface IPlayerRepository extends MongoRepository<Player, Long> {
